10bafb095e616b558877bc892fd765f5dbfa3647
Eli <temp@temps-MacBook-Pro.local>
2013-12-13 08:29:13 -0500

    eddditing and collecting files and data into one
    putt css files in there order and js in its order together to simplify
    my file structure and tags.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 /* =======Solar Compass========= */
+
 $(document).ready(function(){
 	var init = function(){
- 	        checkLoginState();
- 	        console.log("hello init");
+ 	        checkLogin();
+ 	        // console.log("hello init");
     };
 	// console.log("runing out of reasons for this not to work"+' '+loadLanding());
 	function loadLanding(){
@@ -10,86 +11,159 @@ $(document).ready(function(){
 		$.get('templates/template.html',function(htmlArg){
 			var land = $(htmlArg).find('#landing-template').html();
 			$.template('landingtemplate', land);
-			var landhtml= $.render('','landingtemplate');
-			console.log("hello load land");
+			var landhtml = $.render('','landingtemplate');
+			// console.log("hello load land");			
 			$('#wrap').append(landhtml);
+			jload();
+			$('#submit_login').on('click', function(e) {
+                e.preventDefault();
+                login();
+            })
 		});
 	};
-	function loadApp(){
-		$.get('templates/template.html',function(htmlArg){
+	var jload=function(){
+		$(function() {		
+			$( ".accordion" ).accordion({collapsible: true});
+			$( "#button" ).button();
+			$( "#radioset" ).buttonset();
+			$( "#tabs" ).tabs();
+			$( "#dialog" ).dialog({
+				autoOpen: false,
+				width: 400,
+				buttons: [
+					{
+						text: "Ok",
+						click: function() {
+							$( this ).dialog( "close" );
+						}
+					},
+					{
+						text: "Cancel",
+						click: function() {
+							$( this ).dialog( "close" );
+						}
+					}
+				]
+			});
+			// Link to open the dialog
+			$( "#dialog-link" ).click(function( event ) {
+				$( "#dialog" ).dialog( "open" );
+				event.preventDefault();
+			});
+			$( "#datepicker" ).datepicker({
+				inline: true
+			});
+			$( "#slider" ).slider({
+				range: true,
+				values: [ 17, 67 ]
+			});
+			$( "#progressbar" ).progressbar({
+				value: 20
+			});
+			// Hover states on the static widgets
+			$( "#dialog-link, #icons li" ).hover(
+				function() {
+					$( this ).addClass( "ui-state-hover" );
+				},
+				function() {
+					$( this ).removeClass( "ui-state-hover" );
+				}
+			);
+			 $(function() {
+		    var icons = {
+		      header: "ui-icon-circle-arrow-e",
+		      activeHeader: "ui-icon-circle-arrow-s"
+		    };
+		    $( ".accordion" ).accordion({
+		      icons: icons
+		    });
+		    $( "#toggle" ).button().click(function() {
+		      if ( $( ".accordion" ).accordion( "option", "icons" ) ) {
+		        $( ".accordion" ).accordion( "option", "icons", null );
+		      } else {
+		        $( ".accordion" ).accordion( "option", "icons", icons );
+		      }
+		    });
+		  });
+		});
+	}
+	var loadApp = function(){
+		console.log("hello load app function");
+        $('#wrap').empty();
+        $.get('templates/template.html',function(htmlArg){
 			var app = $(htmlArg).find('#app-template').html();
 			$.template('apptemplate', app);
 			var html= $.render('','apptemplate');
 			console.log("load app");
 			$('#wrap').append(html);
-		});
-	};
-	var loadApp = function(){
-				console.log("hello load app function");
-                $('#wrap').empty();
-                $.get('templates/template.html',function(htmlArg){
-					var app = $(htmlArg).find('#app-template').html();
-					$.template('apptemplate', app);
-					var html= $.render('','apptemplate');
-					console.log("load app");
-					$('#wrap').append(html);
-                    //logout button
-                    $('#log-out').on('click', function(e){
-                            e.preventDefault();
-                            $.get('xhr/logout.php', function(){
-                                    loadLanding();
-                            });
-                    })
-                    return false;
+            //logout button
+            $('#logout').on('click', function(e){
+                e.preventDefault();
+                $.get('xhr/logout.php', function(){
+                    loadLanding();
                 });
-                $.get('templates/app.html', function(html){
-                        var appCode = $(html).find('#app-temp').html();
-                        $.template('appTemp', appCode);                // compile template
-                        var appTemp = $.render('', 'appTemp');                // use template
-                        container.append(appTemp);
-                        getProjects();
-                });
+            logout();
+
+            });
+            jload();
+            return false;
+        });
     };
 	<!-- ===================================== login function =================================== -->
-	var checkLoginState = function(){
-		console.log("shit.... its loading this at least")
-                $.ajax({
-                        url: 'xhr/check_login.php',
-                        type: 'get',
-                        dataType: 'json',
-                        success: function(response){
-                                // if user, loadApp()
-                                // if error, loadLanding()
-                                if(response.user){
-                                        loadApp();
-                                        console.log("login to app");
-                                } else {
-                                        loadLanding();
-                                        console.log("loadlanding log check state");
-                                }
-                        }
-                });
-        };
-	function login(){
-		var username=$('#username').val();
-		var pass =$('#password').val();
+	var checkLogin = function(){
 		$.ajax({
-			url:'xhr/login.php',
-			data:{
-				user:username,
-				pass:pass
-			},
-			type:'post',
-			dataType:"json",
-			success:function(response){
-				console.log(response);
+			url:'xhr/check_login.php',
+			type: 'get',
+			dataType: 'json',
+			success: function(response){
 				if(response.user){
 					loadApp();
 				}else{
 					loadLanding();
+				};
+			}
+		});
+	}
+	function login(){
+		console.log("login start");
+		var username=$('#username').val();
+		var pass =$('#password').val();
+		$.ajax({
+			url: 'xhr/login.php',
+			data: {
+				username: username,
+				password: pass
+			},
+			type: 'post',
+			dataType: 'json',
+			success: function(response){
+				if(response.error){
+					showLoginError();
+				}else{
+					loadApp();
+
 				}
 			}
 		});
 	};
 	init();
+	function pget(){
+		$.ajax({
+			url: 'xhr/get_projects.php',
+			data: {
+				projectID: "user"
+			},
+			type: 'post',
+			dataType: 'json',
+			success: function(response){
+				if(response.error){
+					console.log(response);
+					showLoginError();
+				}else{
+					console.log(response);
+					loadApp();
+				}
+			}
+		});
+	}
 });
